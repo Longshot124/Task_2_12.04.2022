74af0c94b9d7986b391427cee905df93053312c5
kamra <kamra@LAPTOP-M49P7DNF>
2022-04-13 02:27:42 +0400

    Добавьте файлы проекта.

---
+++ Task_2_12.04.2022/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Task_2_12._04._2022
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1-ci reqemi daxil edin");
+            int FirstNumber = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("2-ci reqemi daxil edin");
+            int SecondNumber = Convert.ToInt16(Console.ReadLine());
+            if (FirstNumber > SecondNumber)
+            {
+                Console.WriteLine("First number" + " " + FirstNumber);
+            }
+            else if (SecondNumber > FirstNumber)
+            {
+                Console.WriteLine("Second number" + " " + SecondNumber);
+            }
+            else
+            {
+                Console.WriteLine("Reqemler Beraberdir =)");
+            }
+        }
+    }
+}
